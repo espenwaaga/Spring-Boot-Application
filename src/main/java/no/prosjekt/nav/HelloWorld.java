e94b777dccb79092eaec873b8cf5c241f27801a0
espenwaaga <espen_250293@hotmail.no>
2019-09-20 16:05:09 +0200

    introduced compile error to code

--- src/main/java/no/prosjekt/nav/HelloWorld.java
+++ src/main/java/no/prosjekt/nav/HelloWorld.java
@@ -2,6 +2,6 @@ package no.prosjekt.nav;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Hello!")
     }
 }
