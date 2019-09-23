363e30500c628e75d34c4bdd021e104e710bba08
espenwaaga <espen_250293@hotmail.no>
2019-09-23 17:07:05 +0200

    Added compability with two DB

---
+++ core/src/main/java/no/prosjekt/config/PersistenceConfiguration.java
@@ -0,0 +1,39 @@
+package no.prosjekt.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.internal.database.h2.H2Database;
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSourceProperties firstDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSource firstDataSource() {
+        return firstDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix="datasource.flyway")
+    @FlywayDataSource
+    public DataSource flywayDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+
+}
